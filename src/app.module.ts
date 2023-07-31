56197f26b4245b3aac68b265950ba83f3f3fbab0
lifeodyssey <zhouthepassion@outlook.com>
2023-07-31 17:13:49 +0800

    feat:create new cart

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CheckoutModule } from "src/checkout/checkout.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [CheckoutModule],
+  imports: [CheckoutModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import axios, { AxiosRequestConfig } from "axios";
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return "Hello World!";
+  }
+
+  async getOrCreatNewCart(): Promise<any> {
+    const options = {
+      method: "get",
+      baseURL: `https://${process.env.accountName}.${process.env.accountEnvironment}.com.br`,
+      url: "/api/checkout/pub/orderForm?forceNewCart=true",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-VTEX-API-AppKey": process.env.X_VTEX_API_AppKey,
+        "X-VTEX-API-AppToken": process.env.X_VTEX_API_AppToken,
+      },
+    } as AxiosRequestConfig;
+
+    const response = await axios.request(options);
+    return response.data;
   }
 }
